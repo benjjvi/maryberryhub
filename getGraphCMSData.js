66b668dc43a6855251bdf8b4696aa7a209b65a42
bildsben <kcliix@cyberservices.com>
2021-09-17 16:54:53 +0100

    nothings fixed i want to die

--- getGraphCMSData.js
+++ getGraphCMSData.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const kasdfsdg = JSON.stringify({
+  const data = JSON.stringify({
     query: `query data {
       contents(orderBy: id_ASC) {
         description
@@ -10,23 +10,24 @@ async function getData() {
     }`,
   });
 
-  const sldfglASFLASDKF = await fetch(
+  const response = await fetch(
     'https://api-eu-central-1.graphcms.com/v2/cktndqghp05z901xm7mhgh7ow/master',
     {
       method: 'post',
-      body: kasdfsdg,
+      body: data,
       headers: {
         'Content-Type': 'application/json',
-        'Content-Length': kasdfsdg.length,
+        'Content-Length': data.length,
         'User-Agent': 'Node',
       },
     }
   );
 
-  const fslkgjsdgdf = await sldfglASFLASDKF.json()
-  const sdlkadfjgjsdflgksdflgkj = await fslkgjsdgdf
-  return sdlkadfjgjsdflgksdflgkj
+  const extradata = await response.json()
+  const information = await extradata
+  return information
 }
 
 let newdata = getData();
 console.log(newdata);
+console.log(newdata.data.contents[0].video.urls)
